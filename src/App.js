d6e3cc55ba94027249fa7a2cca3e0a3ee710bfae
Brandon Portheine <bportheine@gmail.com>
2018-04-22 20:43:14 +0200

    return component to app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <h1>Particle Test</h1>
+        <Particles params={{
+            		particles: {
+            			line_linked: {
+            				shadow: {
+            					enable: true,
+            					color: "#fff",
+            					blur: 5
+            				}
+            			}
+            		}
+            	}}
+              style={{
+                width: '100%',
+                backgroundColor: `#000`
+              }}/>
       </div>
     );
   }
